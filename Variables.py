3a986ddde8b9b89097de5cfbe7346d331e2ed86c
moonbune <59174070+moonbune@users.noreply.github.com>
2019-12-23 15:09:36 +0000

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,10 @@
+character_name = "Luke"
+character_age = "25"
+is_male = True
+print("There once was a man named " + character_name + ",")
+print("He was " + character_age + " years old. ")
+
+character_name = "alfie"
+character_age = "15"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
