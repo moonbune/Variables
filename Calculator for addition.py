5801f73efd5067f8aba138b5251bc53eae3c1896
Moonbune <59174070+moonbune@users.noreply.github.com>
2019-12-25 22:20:58 +0000

    Update Calculator for addition.py

--- Calculator for addition.py
+++ Calculator for addition.py
@@ -3,7 +3,7 @@ num2 = input("Enter another whole number!")
 result = int(num1) + int(num2)
 print(result)
 
-num3 = input("Enter a number decimal number!")
+num3 = input("Enter a decimal number!")
 num4 = input("Enter another decimal number!")
 result2 = float(num3) + float(num4)
 
