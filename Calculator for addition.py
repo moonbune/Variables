fc6ddce046b9ad83e66dca028e599b48f37d819d
Moonbune <59174070+moonbune@users.noreply.github.com>
2019-12-25 22:18:54 +0000

    Add files via upload

---
+++ Calculator for addition.py
@@ -0,0 +1,12 @@
+num1 = input("Enter a whole number!")
+num2 = input("Enter another whole number!")
+result = int(num1) + int(num2)
+print(result)
+
+num3 = input("Enter a number decimal number!")
+num4 = input("Enter another decimal number!")
+result2 = float(num3) + float(num4)
+
+print(result2)
+
+
